2d80e8d4b01f3e71ac4b6911814d58f3ef02661f
Jake1152 <rancho974@gmail.com>
2024-06-15 00:11:54 +0900

    Refactor : MinAndMax
    - use swap(), min() and max()

--- CPP/HongLab/ch04_medium_value_and_selection/MinAndMax/MinAndMax.cpp
+++ CPP/HongLab/ch04_medium_value_and_selection/MinAndMax/MinAndMax.cpp
@@ -10,16 +10,19 @@ using namespace std;
 int main()
 {
 	// vector<int> arr = { 8, 2, 3, 5, 9, 1, 9, 4, 3, 7, 6, 7 };
-	random_device rd;
-	mt19937 gen(rd());
-
-	const int n = 20;
-	vector<int> my_vector(n);
-	
-	uniform_int_distribution<int> value_distribution(1, 10);
-	generate(my_vector.begin(), my_vector.end(), [&]() { return value_distribution(gen); });
 
 	// TODO:
+	// # Mine
+	/* 
+	// random_device rd;
+	// mt19937 gen(rd());
+
+	// const int n = 20;
+	// vector<int> my_vector(n);
+	
+	// uniform_int_distribution<int> value_distribution(1, 10);
+	// generate(my_vector.begin(), my_vector.end(), [&]() { return value_distribution(gen); });
+
 	int max = my_vector[0];
 	int min = my_vector[0];
 
@@ -48,5 +51,38 @@ int main()
 	std::cout << std::endl;
 	assert(min == my_vector[0]);
 	assert(max == my_vector.at(my_vector.size() - 1));
-	
+	*/
+
+	// # Honglab
+    vector<int> arr = { 8, 2, 3, 5, 9, 1, 9, 4, 3, 7, 6, 7 };
+    if (arr[0] > arr[1])
+        swap(arr[0], arr[1]);
+
+    for (int i = 0; i < arr.size(); i += 2)
+    {
+        if (arr[i] > arr[i + 1])
+            swap(arr[i], arr[i + 1]);
+
+        arr[0] = std::min(arr[i], arr[0]);      // min() 내부에서 비교 1번
+        arr[1] = std::max(arr[i + 1], arr[1]);  // max() 내부에서 비교 1번
+
+        // min(), max() 대신에 if 사용하셨어도 괜찮습니다.
+
+        // swap을 사용하지 않을 경우
+        //if (arr[i] < arr[i + 1])
+        //{
+        //    arr[0] = std::min(arr[i], arr[0]);
+        //    arr[1] = std::max(arr[i + 1], arr[1]);
+        //}
+        //else
+        //{
+        //    arr[0] = std::min(arr[i + 1], arr[0]);
+        //    arr[1] = std::max(arr[i], arr[1]);
+        //}
+    }
+    cout << "Min value = " << arr[0] << ", Max value = " << arr[1] << endl;
+
+
+
+	return 0;
 }
