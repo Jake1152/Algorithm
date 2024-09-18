5fb1c21e547f036edb370cf5e3fe71098dbf76b7
Jake1152 <rancho974@gmail.com>
2024-09-18 20:39:16 +0900

    feat: Fractional Knapsack

--- CPP/HongLab/ch14_Greedy/FractionalKnapsack/FractionalKnapsack.cpp
+++ CPP/HongLab/ch14_Greedy/FractionalKnapsack/FractionalKnapsack.cpp
@@ -37,9 +37,31 @@ double FractionalKnapsack(vector<Item> items, double W)
 
 	double vsum = 0.0;
 
+	/**
+	 * weight에 반영된 item은 제거하거나
+	 * item의 가치를 무한대로 만든다.
+	 * for문만 돌아도 충분히 계산가능.
+	 * 전체 아이템 수이기에 이미 계산된 item을 리스트에서 제외할 필요는 없다.
+	 * 
+	 * 무게 단위별로 가져올 수 있다는 특징이 있다
+	 * 가치 6짜리 아이템의 무게가 2일 때
+	 * 무게 1을 가져오고 가치 3을 얻을 수 있다.
+	 */
 	for (auto& i : items)
 	{
 		// TODO:
+		/**
+		 * min(현재 베낭 남은 무게, 아이템 무게)
+		 * 몫 * 가치 
+		 */
+		if (W <= 0)
+			break ;
+		const size_t available_weight = min(W, i.weight);
+		std::cout << "W : " << W << std::endl;
+		W -= available_weight;
+		std::cout << "available_weight : " << available_weight << std::endl;
+		const double ratio_item = i.value / i.weight;
+		vsum += ratio_item * available_weight;
 	}
 
 	return vsum;
